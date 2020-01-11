4d0c9c2ce1e486b44b545d753fa3f4e2571c6ac0
Aidan Tong <aidan.tong@berkeley.edu>
2020-01-11 16:13:43 +0800

    defined environment variables

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
+// load environment
+dotenv.config({ path: './config.env' });
+
+const app = express(); // creates express server
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+})
