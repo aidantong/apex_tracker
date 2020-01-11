2337ac98c30146f071696786ab2346187b80c585
Aidan Tong <aidan.tong@berkeley.edu>
2020-01-11 16:57:17 +0800

    moved routes to separate file. basic backend compelete

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const dotenv = require('dotenv');
 // load environment
 dotenv.config({ path: './config.env' });
 
-const app = express(); // creates express server
+// creates express server
+const app = express(); 
+
+// dev logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// profile routes
+app.use('/api/v1/profile', require('./routes/profile'));
 
 const port = process.env.PORT || 8000
 
