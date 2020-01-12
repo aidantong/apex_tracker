4953446ff2c6d4296ce55e8cdb2e8eacfbae319f
Aidan Tong <aidan.tong@berkeley.edu>
2020-01-12 18:09:48 +0800

    completed Search vue component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Header />
+    <router-view />
   </div>
 </template>
 
@@ -9,7 +10,7 @@ import Header from './components/Header';
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
   }
 };
 </script>
