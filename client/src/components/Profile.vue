b288093a49b6021907cd15328ceb9368a3af5428
Aidan Tong <aidan.tong@berkeley.edu>
2020-01-13 17:36:45 +0800

    completed Profile vue component

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -24,16 +24,38 @@
                             <h4>Selected Legend</h4>
                             <p>{{profileData.metadata.activeLegendName}}</p>
                         </li>
-                        <li>
+                        <li v-if="profileData.segments[0].stats.season2Wins">
                             <h4>Season 2 Wins</h4>
                             <p>
                                 {{profileData.segments[0].stats.season2Wins.displayValue}}
                                 <span>({{profileData.segments[0].stats.season2Wins.percentile}}%)</span>
                             </p>
                         </li>
+                        <li v-if="profileData.segments[0].stats.level">
+                            <h4>Apex Level</h4>
+                            <p>
+                                {{profileData.segments[0].stats.level.displayValue}}
+                                <span>({{profileData.segments[0].stats.level.percentile}}%)</span>
+                            </p>
+                        </li>
+                        <li v-if="profileData.segments[0].stats.kills">
+                            <h4>Lifetime Kills</h4>
+                            <p>
+                                {{profileData.segments[0].stats.kills.displayValue}}
+                                <span>({{profileData.segments[0].stats.kills.percentile}}%)</span>
+                            </p>
+                        </li>
+                        <li v-if="profileData.segments[0].stats.damage">
+                            <h4>Damage Done</h4>
+                            <p>
+                                {{profileData.segments[0].stats.damage.displayValue}}
+                                <span>({{profileData.segments[0].stats.damage.percentile}}%)</span>
+                            </p>
+                        </li>
                     </ul>
                 </div>
             </div>
+            <router-link to="/">Go Back</router-link>
         </div>
     </section>    
 </template>
